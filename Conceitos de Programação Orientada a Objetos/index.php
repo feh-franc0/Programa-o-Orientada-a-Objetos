a97ad48ba59515624642aaea8e37cf528b7b8af9
feh-franc0 <fernandofv1110@gmail.com>
2022-01-06 19:04:08 -0300

    Conceitos de Programação Orientada a Objetos

---
+++ Conceitos de Programação Orientada a Objetos/index.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * !Conceitos da Programação Orientada a Objeto :
+ * *É um estilo de programação, algumas linguagem de programação são totalmentes orietadas a objeto (ex:java)
+ * 
+ * !IREMOS VER NESSE DIRETORIO:
+ * *Criando uma Classe, Atributos e Métodos, Métodos Estáticos, Métodos Mágicos, Encapsulamento, Herança, Interface, Classe Abstrata, Polimorfismo, Incluindo classes com Autoload, Usando Namespace.
+ * 
+ * TODO:Conheça os conceitos BASICOS:
+ * 
+ * !CLASSE:
+ * ?Classe é uma caixa que o que importa é o que esta dentro dela.Então sempre que for criar um assunto faça uma CLASSE que so ira falar daquele assunto(ex:na escola tem a classe de portugues onde se ira falar apenas de portugues,se voce quiser falar de matemarica voce ira ter que ir para a classe de matematica).A Classe so vai proteger as informaçoes que estao dentro dela, temos dentro da classe os atributos e metodos 
+ * ?('variaveis' com mais recurso = ATRIBUTO . Temos atributos privados e publicos(ENCAPSULAMENTO), so ira poder ver o atributo privado quem esta dentro da classe  )
+ * ?('funcões' com mais recursos = METODOS . Temos metoros privados e publicos (ENCAPSULAMENTO)), Executa o que precisamos 
+ * 
+ * 
+ * !ENCAPSULAMENTE:
+ * ? Quem pode ou nao pode ter acesso ao Atributo ou Metodo
+ * 
+ * 
+ * !OBJETO = variavel que representa ou tira uma copia da classe.
+ * ? na Hora de usarmos uma classe precisamos instanciar-la(instancia = quando de fato iremos usar a classe), 
+ * *OBJETO=($sql = new Classe SQL())
+ * 
+ * 
+ */
+
